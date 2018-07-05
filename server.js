b20bababd47ce240f94b2ce11abfaec2072c9cc2
mariko-nakajo <39357142+mariko-nakajo@users.noreply.github.com>
2018-07-05 17:22:16 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+// nodeのコアモジュールのhttpを使う
+var http = require('http');
+var server = http.createServer();
+
+server.on('request', function(req, res) {
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    // リクエストされた処理により表示内容を分けてみる
+    switch (req.url) {
+        case '/about':
+            msg = 'welcome about page';
+            break;
+        case '/company':
+            msg = 'welcome my company page';
+            break;
+        default:
+            msg = 'page not found';
+            break;
+    }
+    res.write(msg + "\n");
+    res.write('hello world\n');
+    res.write('request from: ' + req.url);
+    res.end();
+});
+
+// サーバを待ち受け状態にする
+// 第1引数: ポート番号
+// 第2引数: IPアドレス
+server.listen(3000);
